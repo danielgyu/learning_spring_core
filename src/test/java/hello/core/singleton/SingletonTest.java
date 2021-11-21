995e090e21f66d510e27e5c8686f76354e04b8e8
kungyu <hismayfly@gmail.com>
2021-11-21 19:30:26 +0900

    adding singleton test

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -24,5 +26,16 @@ public class SingletonTest {
         SingletonService singletonService1 = SingletonService.getInstance();
         SingletonService singletonService2 = SingletonService.getInstance();
         Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("Singleton Spring container")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService ms1 = ac.getBean("memberService", MemberService.class);
+        MemberService ms2 = ac.getBean("memberService", MemberService.class);
+
+        Assertions.assertThat(ms1).isSameAs(ms2);
     }
 }
